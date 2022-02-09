5d541f16e5eddd0c9496dc9779147b8f3e303ab7
Pablo Núñez <pablonete@github.com>
2022-02-09 19:51:12 +0100

    Use resources to generate all routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "/movies", to: "movies#index"
-  get "/movies/:id", to: "movies#show", as: "movie"
+  # get "/movies", to: "movies#index"
+  # get "/movies/:id", to: "movies#show", as: "movie"
+  resources :movies, only: [:index, :show]
 end
