a6754a5dd1d44a4585a263649098ecd1660c6a17
Pablo Núñez <pablonete@github.com>
2022-02-09 18:30:03 +0100

    Write tests for the new methods.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,22 +3,22 @@ class Movie < ApplicationRecord
 
   # Make a method that returns an array of just movie titles.
   def self.titles
-    Movie.pluck(:title)
+    pluck(:title)
   end
 
   # Make a method that returns all movies in order of most to least Facebook likes.
   def self.sort_by_likes
-    Movie.order(facebook_likes: :desc)
+    order(facebook_likes: :desc)
   end
 
   # Make a method that can find a movie by its title, and tell you what year it was released.
   def self.year_by_title(title)
-    Movie.find_by(title: title)&.year
+    find_by(title: title)&.year
   end
 
   # Make a method that returns the number of movies in the database with more than a given number of Facebook likes
-  def self.more_likes(likes)
+  def self.count_above_likes(likes)
     # Movie.where(facebook_likes: (likes + 1)..)
-    Movie.where("facebook_likes > ?", likes)
+    where("facebook_likes > ?", likes).count
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -16,4 +16,35 @@ class MovieTest < ActiveSupport::TestCase
     movie = Movie.new(director: "Test")
     refute_predicate movie, :valid?
   end
+
+  test 'list movie titles' do
+    Movie.create!(title: "Movie1")
+    Movie.create!(title: "Movie2")
+
+    assert_equal ["Movie1", "Movie2"], Movie.titles
+  end
+
+  test 'sort movies by likes' do
+    a = Movie.create!(title: "a", facebook_likes: 100)
+    c = Movie.create!(title: "c", facebook_likes: 3)
+    b = Movie.create!(title: "b", facebook_likes: 20)
+
+    assert_equal [a, b, c], Movie.sort_by_likes
+  end
+
+  test 'get year by movie title' do
+    Movie.create!(title: "Interstellar", year: "2018")
+    
+    assert_equal "2018", Movie.year_by_title("Interstellar")
+
+    assert_nil Movie.year_by_title("unknown title")
+  end
+
+  test 'get movies with more than certain likes' do
+    a = Movie.create!(title: "a", facebook_likes: 100)
+    c = Movie.create!(title: "c", facebook_likes: 3)
+    b = Movie.create!(title: "b", facebook_likes: 20)
+
+    assert_equal 1, Movie.count_above_likes(20)
+  end
 end
