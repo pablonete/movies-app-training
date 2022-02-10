f222d2c11b0948f1f4b41a6126c9f97d0e9ac424
Pablo Núñez <pablonete@github.com>
2022-02-10 20:30:43 +0100

    Create color enum.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_182130) do
+ActiveRecord::Schema.define(version: 2022_02_10_185919) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2022_02_08_182130) do
     t.string "director"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "color_format", default: 0
   end
 
 end
